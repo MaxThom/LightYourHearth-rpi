3b50fb171aa953a0787433127a63536876e69106
MaxThom <maxthomassin@hotmail.com>
2020-08-24 18:25:21 -0400

    added new lib

--- RpiServer/led_animation_util.py
+++ RpiServer/led_animation_util.py
@@ -1,30 +1,41 @@
-# Simple demo of of the WS2801/SPI-like addressable RGB LED lights.
 import time
-# Import the WS2801 module.
-import Adafruit_WS2801
-import Adafruit_GPIO.SPI as SPI 
+import time
+from rpi_ws281x import Color, PixelStrip, ws
  
+def clear(pixels, must_show=True):
+    for i in range(pixels.numPixels()):
+        pixels.setPixelColor(i, Color(0, 0, 0))
+    if (must_show): pixels.show()
+    
+
 # Define the wheel function to interpolate between different hues.
 def wheel(pos):
     if pos < 85:
-        return Adafruit_WS2801.RGB_to_color(pos * 3, 255 - pos * 3, 0)
+        return Color(pos * 3, 255 - pos * 3, 0)
     elif pos < 170:
         pos -= 85
-        return Adafruit_WS2801.RGB_to_color(255 - pos * 3, 0, pos * 3)
+        return Color(255 - pos * 3, 0, pos * 3)
     else:
         pos -= 170
-        return Adafruit_WS2801.RGB_to_color(0, pos * 3, 255 - pos * 3)
+        return Color(0, pos * 3, 255 - pos * 3)
  
+def color_wipe(pixels, isCancelled, wait=0.0, color=(255,255,255)):
+    for i in range(pixels.numPixels()):
+        pixels.setPixelColor(i, Color(color[2], color[1], color[0] ))
+        if (wait != 0.0):
+            time.sleep(wait)
+            pixels.show()
+    pixels.show()
+
 # Define rainbow cycle function to do a cycle of all hues.
 def rainbow_cycle_successive(pixels, isCancelled, wait=0.1):
-    pixels.clear()
-    pixels.show()
-    for i in range(pixels.count()):
+    clear(pixels)
+    for i in range(pixels.numPixels()):
         # tricky math! we use each pixel as a fraction of the full 96-color wheel
         # (thats the i / strip.numPixels() part)
         # Then add in j which makes the colors go around per pixel
         # the % 96 is to make the wheel cycle around
-        pixels.set_pixel(i, wheel(((i * 256 // pixels.count())) % 256) )
+        pixels.setPixelColor(i, wheel(((i * 256 // pixels.numPixels())) % 256) )
         if (isCancelled()):
             break
         pixels.show()
@@ -34,11 +45,10 @@ def rainbow_cycle_successive(pixels, isCancelled, wait=0.1):
             break
  
 def rainbow_cycle(pixels, isCancelled, wait=0.005):
-    pixels.clear()
-    pixels.show()
+    clear(pixels)
     for j in range(256): # one cycle of all 256 colors in the wheel
-        for i in range(pixels.count()):
-            pixels.set_pixel(i, wheel(((i * 256 // pixels.count()) + j) % 256) )
+        for i in range(pixels.numPixels()):
+            pixels.setPixelColor(i, wheel(((i * 256 // pixels.numPixels()) + j) % 256) )
         if (isCancelled()):
             break
         pixels.show()
@@ -48,11 +58,10 @@ def rainbow_cycle(pixels, isCancelled, wait=0.005):
             break
  
 def rainbow_colors(pixels, isCancelled, wait=0.05):
-    pixels.clear()
-    pixels.show()
+    clear(pixels)
     for j in range(256): # one cycle of all 256 colors in the wheel
-        for i in range(pixels.count()):
-            pixels.set_pixel(i, wheel(((256 // pixels.count() + j)) % 256) )
+        for i in range(pixels.numPixels()):
+            pixels.setPixelColor(i, wheel(((256 // pixels.numPixels() + j)) % 256) )
         if (isCancelled()):
             break
         pixels.show()
@@ -63,12 +72,12 @@ def rainbow_colors(pixels, isCancelled, wait=0.05):
  
 def brightness_decrease(pixels, isCancelled, wait=0.01, step=1):
     for j in range(int(256 // step)):
-        for i in range(pixels.count()):
-            r, g, b = pixels.get_pixel_rgb(i)
+        for i in range(pixels.numPixels()):
+            r, g, b = pixels.getPixelColorRGB(i)
             r = int(max(0, r - step))
             g = int(max(0, g - step))
             b = int(max(0, b - step))
-            pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color( r, g, b ))
+            pixels.setPixelColor(i, Color(r, g, b))
         if (isCancelled()):
             break
         pixels.show()
@@ -78,21 +87,19 @@ def brightness_decrease(pixels, isCancelled, wait=0.01, step=1):
             break
  
 def blink_color(pixels, isCancelled, blink_time=5, wait=0.5, color=(255,0,0)):
-    pixels.clear()
-    pixels.show()
+    clear(pixels)
     for i in range(blink_time):
         # blink two times, then wit
-        pixels.clear()
+        clear(pixels)
         for j in range(2):
-            for k in range(pixels.count()):
+            for k in range(pixels.numPixels()):
                 # LedStrip is bgr and not rgb  
-                pixels.set_pixel(k, Adafruit_WS2801.RGB_to_color( color[2], color[1], color[0] ))
+                pixels.setPixelColor(k, Color(color[2], color[1], color[0] ))
             if (isCancelled()):
                 break
             pixels.show()
             time.sleep(0.08)
-            pixels.clear()
-            pixels.show()
+            clear(pixels)
             if (isCancelled()):
                 break
             time.sleep(0.08)
@@ -103,17 +110,16 @@ def blink_color(pixels, isCancelled, blink_time=5, wait=0.5, color=(255,0,0)):
             break
  
 def appear_from_back(pixels, isCancelled, color=(255, 0, 0)):
-    pixels.clear()
-    pixels.show()
+    clear(pixels)
     pos = 0
-    for i in range(pixels.count()):
-        for j in reversed(range(i, pixels.count())):
-            pixels.clear()
+    for i in range(pixels.numPixels()):
+        for j in reversed(range(i, pixels.numPixels())):
+            clear(pixels, False)
             # first set all pixels at the begin
             for k in range(i):
-                pixels.set_pixel(k, Adafruit_WS2801.RGB_to_color( color[2], color[1], color[0] ))
+                pixels.setPixelColor(k, Color( color[2], color[1], color[0] ))
             # set then the pixel at position j
-            pixels.set_pixel(j, Adafruit_WS2801.RGB_to_color( color[2], color[1], color[0] ))            
+            pixels.setPixelColor(j, Color( color[2], color[1], color[0] ))            
             if (isCancelled()):
                 break
             pixels.show()
--- RpiServer/main.py
+++ RpiServer/main.py
@@ -11,7 +11,7 @@ import datetime
 # pip freeze > requirements.txt
 # pip install -r requirements.txt
 
-pixels = Ledstrip(constants.PIXEL_COUNT, constants.SPI_PORT, constants.SPI_DEVICE)
+pixels = Ledstrip()
 blue_comm = None
 
 def main():
@@ -72,6 +72,7 @@ commandAction = {
         constants.LED_BRIGHTNESS_DECREASE: pixels.pixel_brightness_decrease,
         constants.LED_BLINK_COLOR: pixels.pixel_blink_color,
         constants.LED_APPEAR_FROM_BACK: pixels.pixel_appear_from_back,
+        constants.LED_COLOR_WIPE: pixels.pixel_color_wipe,
         constants.LED_SETTINGS: pixels.set_settings,
         constants.LED_ANIMATION_CAPABILITIES: get_server_capabilities
     }
